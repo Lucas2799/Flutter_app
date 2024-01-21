665eb5499574b770c5cd6c5081afcb06e9e78acf
lucas2799 <ltv2234@gmail.com.br>
2024-01-21 17:37:35 -0300

    APP_Flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(new Perguntas());
+}
+
+class Perguntas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: ('Meu primeiro APP'), home: Text('Ola mundo'));
+  }
+}
